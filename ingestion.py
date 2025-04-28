4d6da7cd118cd42b7ad56ed865003180535292dd
N Chinna reddy <defender2106@gmail.com>
2025-04-29 00:09:08 +0530

    Ingestion.py

---
+++ ingestion.py
@@ -0,0 +1,19 @@
+import pyspark
+from pyspark.sql import SparkSession
+
+class Ingest:
+    def __init__(self,spark):
+        self.spark=spark
+
+
+    def ingest_data(self):
+        print("Ingetion data from local source")
+        customerdf=self.spark.read.csv("file:////C:/Bigdata_Mithun/Pyspark/Documents/DATASETS/usdata.csv",header= True)
+        customerdf.show()
+        return customerdf
+
+
+
+
+
+
